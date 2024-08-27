fc3744b41b53591e30e21fc5487a32aa870d5c76
Geoff Franks <geoff.franks@broadcom.com>
2024-08-27 20:39:57 +0000

    more fixing

--- task.bash
+++ task.bash
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-set -eu
+set -eux
 
 
 . ci/shared/helpers/git-helpers.bash
 
+pushd repo
 
-echo "$CONTENT" >> "repo/$FILE"
+echo "$CONTENT" >> "$FILE"
 
 git_configure_author
 git add "$FILE"
 git commit -m "Added $CONTENT to $FILE"
 
+cp -r . ../updated-repo
+popd
